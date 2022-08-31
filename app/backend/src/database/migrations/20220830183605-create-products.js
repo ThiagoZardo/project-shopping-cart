daeb983a44b8af86017dd102bf9a77e79be1dd42
ThiagoZardo <thiago.artevisual@gmail.com>
2022-08-31 14:52:36 -0300

    Criei a migration e o seeder para os produtos

---
+++ app/backend/src/database/migrations/20220830183605-create-products.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      product_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('products');
+  }
+};
